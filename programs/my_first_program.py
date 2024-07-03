8bd228e1a074402b67b7656179e2442847f7cebd
prathanab <prathanab30@gmail.com>
2024-07-03 07:18:36 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,37 +1,23 @@
 from nada_dsl import *
 
-def enable_telemetry():
-    telemetry_id = "unique-identifier-5678"
-    Telemetry.enable(identifier=telemetry_id)
-    return telemetry_id
-
 def nada_main():
-    telemetry_id = enable_telemetry()
+    party1 = Party(name="Party1")
     
-    voter1 = Party(name="Voter1")
-    voter2 = Party(name="Voter2")
-    voter3 = Party(name="Voter3")
-    outparty = Party(name="OutParty")
+    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
     
-    vote1_candidate1 = SecretInteger(Input(name="vote1_candidate1", party=voter1))
-    vote1_candidate2 = SecretInteger(Input(name="vote1_candidate2", party=voter1))
-    vote2_candidate1 = SecretInteger(Input(name="vote2_candidate1", party=voter2))
-    vote2_candidate2 = SecretInteger(Input(name="vote2_candidate2", party=voter2))
-    vote3_candidate1 = SecretInteger(Input(name="vote3_candidate1", party=voter3))
-    vote3_candidate2 = SecretInteger(Input(name="vote3_candidate2", party=voter3))
+    sum_int = my_int1 + my_int2
+    diff_int = my_int1 - my_int2
+    prod_int = my_int1 * my_int2
+    div_int = my_int1 / my_int2
+    max_int = (my_int1 < my_int2).if_else(my_int2, my_int1)
+    min_int = (my_int1 < my_int2).if_else(my_int1, my_int2)
     
-    total_votes_candidate1 = vote1_candidate1 + vote2_candidate1 + vote3_candidate1
-    total_votes_candidate2 = vote1_candidate2 + vote2_candidate2 + vote3_candidate2
+    output_sum = Output(sum_int, "sum_output", party1)
+    output_diff = Output(diff_int, "diff_output", party1)
+    output_prod = Output(prod_int, "prod_output", party1)
+    output_div = Output(div_int, "div_output", party1)
+    output_max = Output(max_int, "max_output", party1)
+    output_min = Output(min_int, "min_output", party1)
     
-    winner = (total_votes_candidate1 < total_votes_candidate2).if_else(total_votes_candidate2, total_votes_candidate1)
-    winner_id = (total_votes_candidate1 < total_votes_candidate2).if_else(2, 1)
-    
-    output_votes_candidate1 = Output(total_votes_candidate1, "total_votes_candidate1", outparty)
-    output_votes_candidate2 = Output(total_votes_candidate2, "total_votes_candidate2", outparty)
-    output_winner = Output(winner, "winner_votes", outparty)
-    output_winner_id = Output(winner_id, "winner_id", outparty)
-    
-    return [output_votes_candidate1, output_votes_candidate2, output_winner, output_winner_id]
-
-telemetry_id = nada_main()[0]
-print(f"Telemetry enabled with identifier: {telemetry_id}")
+    return [output_sum, output_diff, output_prod, output_div, output_max, output_min]
